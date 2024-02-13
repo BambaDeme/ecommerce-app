13817e5ee6fc4f5ab2ff0b0416af68f9754d4dfa
bambademe <cheikhbambademe@gmail.com>
2024-02-13 22:59:01 +0000

    login and logout

--- ecommerce-front/src/app/app-routing.module.ts
+++ ecommerce-front/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 import { ProductsComponent } from './ui/products/products.component';
 
 const routes: Routes = [
-  {path: "products", component: ProductsComponent}
+  {path: "products", component: ProductsComponent, canActivate: [AuthGuard], data: {roles: ['ADMIN']}}
 ];
 
 @NgModule({
--- ecommerce-front/src/app/app.component.ts
+++ ecommerce-front/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
+import { KeycloakProfile } from 'keycloak-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ecommerce-angular';
+  public profile!: KeycloakProfile;
+
+  constructor(public keycloakService: KeycloakService){}
+
+  ngOnInit(): void {
+    if(this.keycloakService.isLoggedIn()){
+      this.keycloakService.loadUserProfile().then(profile => {
+        this.profile = profile;
+      });
+    }
+  }
+
+  logout(): void {
+    this.keycloakService.logout(window.location.origin)
+  }
 }
